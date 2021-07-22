98e2e141b6ec0762ecb0548a224992c5651ba85d
art.gardner <artgardner498@gmail.com>
2021-07-22 19:10:54 +0100

    added more colours

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ TYPES = {"Red": (255, 0, 0),
          "Green": (0, 255, 0),
          "Blue": (0, 0, 255),
          "Yellow": (255, 255, 0),
-         "Purple": (255, 0, 255)}
+         "Purple": (255, 0, 255),
+         "Cyan": (0, 255, 255)}
 
 
 def create_relation():
